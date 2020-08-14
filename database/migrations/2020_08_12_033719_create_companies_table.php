4820b8431e0da7b4ae433555b1688897cd4da0fd
marie1st <69067303+marie1st@users.noreply.github.com>
2020-08-14 13:21:49 +0700

    Add files via upload

---
+++ database/migrations/2020_08_12_033719_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('image');
+            $table->string('file');
+            $table->string('authy_id')->nullable();
+            $table->boolean('verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
