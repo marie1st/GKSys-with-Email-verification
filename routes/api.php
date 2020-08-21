8901a902fe17897257598b22bcb53632a1b46855
marie1st <69067303+marie1st@users.noreply.github.com>
2020-08-21 11:05:52 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
+
+Route::post('/password/email', 'Api\ForgotPasswordController@sendResetLinkEmail');
+Route::post('/password/reset', 'Api\ResetPasswordController@reset');
+
+Route::post('/email/resend', 'Api\VerificationController@resend');
+Route::post('/email/verifiy/{id}/{hash}', 'Api\VerificationController@verify')->name('verification.verify');
